072468c95431f66639a7d07e2e5211c50137cef1
Jaiganesh <jaiganeshv52@gmail.com>
2025-02-03 19:09:38 +0530

    validations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,21 @@
 const express= require("express");
 const app = express();
 const dbConnect = require("./config/database");
+const User = require("./models/user");
+
+app.use(express.json());
+
+app.post("/signup", async (req,res) =>{
+    const user = req.body;
+    const newUser = new User(user);
+    try{
+        await newUser.save();
+        res.send("User Added");
+    }
+    catch(err){
+            res.status(400).send("Error : ", err.message);
+    }
+})
 
 dbConnect()
 .then(()=>{
@@ -13,8 +28,5 @@ dbConnect()
 })
 .catch((err)=>{
     console.log(err);
-})
-app.use("/",(req,res)=>{
-    res.send("hello world");
 })
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,17 +1,53 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     name : {
-        type : String
+        type : String,
+        required : true,
+        minLength : 3,
+        maxLength : 30,
     },
     email : {
-        type : String
+        type : String,
+        required : true,
+        unique : true,
+        trim : true,
+        validate(val){
+            if(!validator.isEmail(val)){
+                throw new Error("Not a Vaild Email!")
+            }
+        }
+
     },
     password : {
-        type : String
+        type : String,
+        required : true,
+        validate(val){
+            if(!validator.isStrongPassword(val)){
+                throw new Error("Enter a Strong password!")
+            }
+        }
     },
+    profile :{
+        default : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+    },
+    gender : {
+        type : String,
+        validate(val){
+            if(!["M","F","O"].includes(val)){
+                throw new Error("Not a Vaild Gender!")
+            }
+        }
+    },
+    skills : {
+        type : Array,
+    },
+},
+{
+    timestamps : true,
 })
 
-const User = mongoose.Model("User",userSchema)
+const User = mongoose.model("User",userSchema)
 
 module.exports = User;
